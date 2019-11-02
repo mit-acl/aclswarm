b20e0ee4a55f4543381a90d240562bdd68c55aef
Parker Lusk <parkerclusk@gmail.com>
2019-11-02 02:46:13 -0400

    added swarm5 gains with fc adjmat

--- aclswarm/matlab/precalc_gains.m
+++ aclswarm/matlab/precalc_gains.m
@@ -43,6 +43,44 @@ disp(A)
 disp(D);
 
 %% swarm5
+n = 5;
+adj = ones(n) - eye(n);  % Complete graph
+
+name = 'Line';
+qs = [[-2.0, 0.0, 0.0];
+      [-4.0, 0.0, 0.0];
+      [+2.0, 0.0, 0.0];
+      [+4.0, 0.0, 0.0];
+      [+0.0, 0.0, 0.0]];
+A = FindGains3D(qs', adj);
+D = squareform(pdist(qs));
+disp(name)
+disp(A)
+disp(D);
+
+name = 'Pentagon';
+qs = [[2.0, 0.0, 0.0];
+      [+0.618, +1.902, 0.0];
+      [-1.618, +1.176, 0.0];
+      [-1.618, -1.176, 0.0];
+      [+0.618, -1.902, 0.0]];
+A = FindGains3D(qs', adj);
+D = squareform(pdist(qs));
+disp(name)
+disp(A)
+disp(D);
+
+name = 'Square+1';
+qs = [[-2., 0.0, 0.0];
+      [+2., 0.0, 0.0];
+      [0.0, +2., 0.0];
+      [0.0, -2., 0.0];
+      [0.0, 0.0, 0.0]];
+A = FindGains3D(qs', adj);
+D = squareform(pdist(qs));
+disp(name)
+disp(A)
+disp(D);
 
 %% swarm6
 
