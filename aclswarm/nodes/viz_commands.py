4fb5117a2a63f2b20fc66d02fbe637f34df41e7a
Parker Lusk <parkerclusk@gmail.com>
2019-12-03 23:45:06 -0500

    added mesh for quadrotors in rviz

--- aclswarm/nodes/viz_commands.py
+++ aclswarm/nodes/viz_commands.py
@@ -6,7 +6,7 @@ import time
 import rospy
 import numpy as np
 
-from acl_msgs.msg import QuadGoal
+from acl_msgs.msg import QuadGoal, ViconState
 from geometry_msgs.msg import Point, Pose, Vector3, Vector3Stamped
 from visualization_msgs.msg import Marker, MarkerArray
 
@@ -22,15 +22,18 @@ class VizCommands:
         # Load desired formations
         self.viz_distcmds = rospy.get_param('~distcmds', True)
         self.viz_safecmds = rospy.get_param('~safecmds', True)
+        self.viz_mesh = rospy.get_param('~mesh', True)
 
         # setup markers
         COLORS = {}
         COLORS['distcmd'] = (0,0,1)
         COLORS['safecmd'] = (1,0,0)
         self.create_arrow_markers(COLORS.keys(), COLORS)
+        self.create_mesh_markers()
 
         # publishers
-        self.pub = rospy.Publisher('viz_cmds', MarkerArray, queue_size=1)
+        self.pub_vizcmds = rospy.Publisher('viz_cmds', MarkerArray, queue_size=1)
+        self.pub_vizmesh = rospy.Publisher('viz_mesh', MarkerArray, queue_size=1)
 
         # subscribe to relevant topics
         self.subs = {}
@@ -44,16 +47,45 @@ class VizCommands:
                 self.subs[veh]['safecmd'] = rospy.Subscriber(
                         "/{}/goal".format(veh), QuadGoal,
                         lambda msg, v=veh: self.safecmdCb(msg, 'safecmd', v))
+            if self.viz_mesh:
+                self.subs[veh]['pose'] = rospy.Subscriber(
+                        "/{}/vicon".format(veh), ViconState,
+                        lambda msg, i=idx, v=veh: self.poseCb(msg, i, v))
 
     def distcmdCb(self, msg, t, veh):
         p = Point(msg.vector.x, msg.vector.y, msg.vector.z)
         self.update_arrow_marker(p, t, veh)
-        self.pub.publish(self.markers)
+        self.pub_vizcmds.publish(self.markers)
 
     def safecmdCb(self, msg, t, veh):
         p = Point(msg.vel.x, msg.vel.y, msg.vel.z)
         self.update_arrow_marker(p, t, veh)
-        self.pub.publish(self.markers)
+        self.pub_vizcmds.publish(self.markers)
+
+    def poseCb(self, msg, vehid, vehname):
+        self.markers_mesh.markers[vehid].pose = msg.pose
+        self.markers_mesh.markers[vehid].header = msg.header
+
+        self.pub_vizmesh.publish(self.markers_mesh)
+
+    def create_mesh_markers(self):
+        self.markers_mesh = MarkerArray()
+
+        for i in range(self.n):
+            m = Marker()
+            m.header = None
+            m.ns = 'mesh'
+            m.id = i*10
+            m.type = Marker.MESH_RESOURCE
+            m.mesh_resource = "package://acl_sim/meshes/quadrotor/quadrotor_base.dae"
+            m.action = Marker.ADD
+            m.color.a = 1
+            m.color.r, m.color.g, m.color.b = (1,1,1)
+            m.pose.orientation.w = 1.0
+            m.scale = Vector3(0.75, 0.75, 0.75)
+            m.lifetime = rospy.Duration(1.0)
+
+            self.markers_mesh.markers.append(m)
 
     def create_arrow_markers(self, topics, c):
         self.markers = MarkerArray()
