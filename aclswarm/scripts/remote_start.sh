4e53175ca6a315cf36241bf4acf45ff4ccb90fc4
Parker Lusk <parkerclusk@gmail.com>
2020-02-04 22:24:03 -0500

    update remote_start

--- aclswarm/scripts/remote_start.sh
+++ aclswarm/scripts/remote_start.sh
@@ -6,19 +6,19 @@ QUAD=$VEH$NUM
 SESSION=remote_$QUAD
 
 if [ -z "$1" ]
-	then
-		echo "No vehicle selected"
-		echo "Example use: ./remote_start.sh SQ 02"
-		exit
-	else
-		echo "Starting vehicle $QUAD"
+    then
+        echo "No vehicle selected"
+        echo "Example use: ./remote_start.sh SQ 02"
+        exit
+    else
+        echo "Starting vehicle $QUAD"
 fi
 
 # check that an ssh connection can even be made
 ssh -q root@$QUAD.local exit
 if [ $? -ne 0 ]; then
-	echo -e "\033[0;31mNo connection to $QUAD\033[0m"
-	exit
+    echo -e "\033[0;31mNo connection to $QUAD\033[0m"
+    exit
 fi
 
 cmd="new-session -d -s $SESSION"
@@ -36,14 +36,14 @@ cmd="$cmd ; split-window -v"
 tmux -2 $cmd
 
 for _pane in $(tmux list-pane -F '#P'); do
-	tmux send-keys -t $SESSION:0.${_pane} "ssh root@$QUAD.local" C-m
-	sleep 1
+    tmux send-keys -t $SESSION:0.${_pane} "ssh root@$QUAD.local" C-m
+    sleep 1
 done
 
 tmux send-keys -t $SESSION:0.4 "imu_app -s 2" C-m
-tmux send-keys -t $SESSION:0.5 "roslaunch snap snap.launch veh:=$VEH num:=$NUM" C-m
-tmux send-keys -t $SESSION:0.0 "roslaunch system_launch quad.launch veh:=$VEH num:=$NUM" C-m
-tmux send-keys -t $SESSION:0.3 "rosrun snap esc_interface_node __ns:=$VEH$NUM" C-m
+tmux send-keys -t $SESSION:0.5 "roslaunch snap snap.launch" C-m
+tmux send-keys -t $SESSION:0.0 "roslaunch outer_loop cntrl.launch" C-m
+tmux send-keys -t $SESSION:0.3 "roslaunch snap esc.launch" C-m
 tmux send-keys -t $SESSION:0.1 "roslaunch aclswarm start.launch veh:=$VEH$NUM"
 #tmux send-keys -t $SESSION:0.2 "roslaunch vislam vislam.launch"
 #tmux send-keys -t $SESSION:0.6 "rosrun aclswarm bagrecord.sh -o $VEH$NUM"
